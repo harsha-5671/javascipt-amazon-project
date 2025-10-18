bf8d1c1b06417d98faca0d26fe7421862aead115
HarshaVardhan Reddy Jalla <jallaharshavardhanreddy24@gmail.com>
2025-10-18 15:53:48 +0530

    Delete products from dom when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -9,7 +9,7 @@ cart.forEach((cartItem)=>{
         if(productId===product.id)
         matchingProduct=product;
      });
-   cartsummaryHTML+=`<div class="cart-item-container">
+   cartsummaryHTML+=`<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -94,6 +94,8 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>
 link.addEventListener('click',()=>{
 const productId=link.dataset.productId;
 removeFromCart(productId);
-console.log(cart);
+const container=document.querySelector(`.js-cart-item-container-${productId}`
+    );
+ container.remove();
 });
 });
